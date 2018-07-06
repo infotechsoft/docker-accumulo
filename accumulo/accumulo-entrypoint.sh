901fda3ed339efb6ba9b4e6c8951e46072c43421
Ray Bradley <rbradley@infotechsoft.com>
2018-07-06 12:44:12 -0400

    [WIP] Initial changes to work with Debian stretch base image (i.e. infotechsoft/hadoop:2.8.4)

--- accumulo/accumulo-entrypoint.sh
+++ accumulo/accumulo-entrypoint.sh
@@ -4,41 +4,18 @@
 export ACCUMULO_MEM_USAGE=${ACCUMULO_MEM_USAGE:-1GB}
 export ACCUMULO_CONF_instance_zookeeper_host=${ACCUMULO_CONF_instance_zookeeper_host:-`hostname -f`:2181}
 
-function addProperty() {
-  local path=$1
-  local name=$2
-  local value=$3
+source /functions.sh
 
-  local entry="<property><name>$name</name><value>${value}</value></property>"
-  local escapedEntry=$(echo $entry | sed 's/\//\\\//g')
-  sed -i "/<\/configuration>/ s/.*/${escapedEntry}\n&/" $path
-}
-
-function configure() {
-  local path=$1
-  local module=$2
-  local envPrefix=$3
-
-  local var
-  local value
-
-  echo "Configuring $module"
-  for c in `printenv | perl -sne 'print "$1 " if m/^${envPrefix}_(.+?)=.*/' -- -envPrefix=$envPrefix`; do
-    name=`echo ${c} | perl -pe 's/___/-/g; s/__/_/g; s/_/./g'`
-    var="${envPrefix}_${c}"
-    value=${!var}
-    echo " - Setting $name=$value"
-    addProperty $path $name "$value"
-  done
-}
-
-$ACCUMULO_HOME/bin/bootstrap_config.sh -s $ACCUMULO_MEM_USAGE -j -v 2
+ACCUMULO_HOME/bin/bootstrap_config.sh -s $ACCUMULO_MEM_USAGE -j -v 2
 
 configure /opt/accumulo/conf/accumulo-site.xml accumulo ACCUMULO_CONF
 
 case $1 in
     init)
         exec /entrypoint.sh /opt/accumulo/bin/accumulo init --instance-name $2 --password $3
+        ;;
+    shell)
+        exec /entrypoint.sh /opt/accumulo/bin/accumulo shell -u $2
         ;;
     *)
         exec /entrypoint.sh $@

f958f3033e0e2d19e03a94aebfce60ac3e99d766
Ray Bradley <rbradley@infotechsoft.com>
2018-07-06 22:33:19 -0400

    Parameterize build

--- build-all.sh
+++ build-all.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
+set -e
 
 if [ $# -gt 0 ]; then
-  ACCUMULO_TAG=$1
+    HADOOP_VERSION=$1
+    if [ $# -gt 1 ]; then
+        ACCUMULO_VERSION=$2
+    fi
 fi
 
-if [ -z ${ACCUMULO_TAG+x} ]; then
-  echo "Must define TAG enviroment variable, or pass as first argument"
+if [ -z ${HADOOP_VERSION+x} ]; then
+  echo "Must define HADOOP_VERSION enviroment variable, or pass as first argument"
   exit 1
 fi
 
-export ACCUMULO_TAG
+if [ -z ${ACCUMULO_VERSION+x} ]; then
+    echo "Must defin ACCUMULO_VERSION environment variable or pass as second argument"
+    exit 1
+fi
 
 for i in accumulo accumulo-gc accumulo-master accumulo-monitor accumulo-tserver; do
     echo Building $i
-    (cd $i && ./build.sh)
+    ( cd $i && \
+      docker build --build-arg HADOOP_VERSION=$HADOOP_VERSION \
+        --build-arg ACCUMULO_VERSION=$ACCUMULO_VERSION \
+        -t infotechsoft/$i:$HADOOP_VERSION-$ACCUMULO_VERSION . )
 done
